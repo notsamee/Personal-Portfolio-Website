7ff30331b6a3de6017379ee8f83cb05906209781
notsamee <notsamee@users.noreply.github.com>
2025-01-13 14:33:53 -0500

    Temporary header change

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,6 @@ function Header() {
                     <li><a href="/#about">About</a></li>
                     <li><a href="/#projects">Projects</a></li>
                     <li><a href="/#skills">Skills</a></li>
-                    <li><a href="/#contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
